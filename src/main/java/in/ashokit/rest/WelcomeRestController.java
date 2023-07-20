83fb7ab6aaadc6b29030344c1c06d82b3cf40d2a
koteswaraRao Sairi <116053113+koteswararao73@users.noreply.github.com>
2023-07-20 10:42:23 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Ashok IT..!!";
+		String msg = "Welcome to Koteswararao Devops Classes..!!";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
